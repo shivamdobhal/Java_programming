4b7697d31e8ed9979fa18b3edc943475cd2481a7
Shivamdobhal <73071952+Shivamdobhal@users.noreply.github.com>
2022-04-17 11:46:56 +0530

    Create for_each_loop.java

---
+++ for_each_loop.java
@@ -0,0 +1,15 @@
+// used to iterate arrays and arraylist 
+public class foreach_loop {
+    public static void main(String args[])
+    {
+        int arr[]={10,20,30,40,50};
+        String a[]={"insane","gr8","RO45","yuvi69"};
+      
+        for(String pubg:a)    //for each loop
+               { System.out.println(pubg);}
+      
+        for(int data:arr)
+               {  System.out.println(data+" ");}
+    }
+                         }
+
